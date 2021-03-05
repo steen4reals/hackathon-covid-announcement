1b1c27f7d27ca46a817019250f2fab1a908e8012
lucaxue <lxue02@gmail.com>
2021-03-05 15:10:27 +0000

    List

--- Frontend/announcements-app/src/Components/List/index.js
+++ Frontend/announcements-app/src/Components/List/index.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+const url = 'http://localhost:5000/announcement';
+
+const announcementsArr = [
+  {
+    date: '2021-03-08T00:00:00',
+    description: 'Schools return, with Covid-19 testing in place.',
+    id: 1,
+    industry: 'Education',
+    title: 'Stage 1a',
+  },
+];
 
 function List() {
-    //announcements state
-    //useEffect for fetching api
-    return (
-        <div>
-            
-        </div>
-    );
+  //announcements state
+  const [announcements, setAnnouncements] = useState(announcementsArr);
+  //useEffect for fetching api
+  useEffect(() => {
+    async function getData() {
+      let res = await fetch(url);
+      let data = await res.json();
+      setAnnouncements(data);
+    }
+    getData();
+  }, []);
+
+  console.log(announcements);
+
+  return (
+    <div>
+      {/* map array of announcements fecthed into ListItems             */}
+    </div>
+  );
 }
 
 export default List;
